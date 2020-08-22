219601dac1665969f5c6b675b247886ecfe8830b
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-22 17:23:55 +0100

    3-user.py

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -11,7 +11,6 @@ class User():
     User class:
     - id: public string unique (uuid)
     - password: private string hash in MD5
-    try
     """
 
     __password = None
@@ -41,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -55,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
